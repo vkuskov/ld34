109529986ab0dee46fc19e3da90f3e60c3e8ba83
Vladimir Kuskov <vladimir.kuskov@hotmail.com>
2015-12-13 10:28:19 +0500

    Level finish trigger

---
+++ LD34/Assets/Code/Finisher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finisher : MonoBehaviour {
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<LeafBackLink>() != null)
+        {
+            Debug.Log("===== GAME FINISHED ====");
+        }
+    }
+}
